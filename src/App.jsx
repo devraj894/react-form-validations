1f91e49767f96fef067ae872192122fb0c54a1ba
Devraj Songara <devrajsongara894@gmail.com>
2025-05-09 18:52:44 +0530

    merge branch 1

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@
 import Form from "./components/React_Hook_Form/Form";
 
 function App() {
+  const merge1 = true;
   return (
    <>
    {/* <Form/> */}
+   <div>Created new branch to practice fork</div>
    <Form/>
    </>
   );
